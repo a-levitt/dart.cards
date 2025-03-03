f7dfef9cb0984c3bbc90f1f0777aae50a83377b8
Artem Trifonov <artemis.levitt@gmail.com>
2025-03-03 18:31:42 +0100

    Start to implement classes

--- cards.dart
+++ cards.dart
@@ -1,12 +1,27 @@
 class Deck {
+  List<Card> cards;
+
+  Deck() {
+    var ranks = ['Six', 'Seven', 'Eight', 'Nine', 'Ten', 
+                'Jack','Queen', 'King', 'Ace'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']; 
   
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        var card = new Card(rank, suit);
+        cards.add(card);
+      }
+    }
+  }
 }
 
 class Card {
-  
+  String suit;
+  String rank;
+
+  Card(this.rank, this.suit);
 }
 
 void main() {
-  
-  }
+   var deck = new Deck();
 }
